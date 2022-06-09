68ccca9d8180037167c24f0000e8e16f351e6c8a
Diego-Cardoso <diegocardososacramento@gmail.com>
2022-06-09 20:46:28 -0300

    atualização de arquivos

---
+++ SPRING-REST-API/forum/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,23 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.dto.TopicoDTO;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicosController {
+
+
+    @RequestMapping(value = "/topicos")
+    public List<TopicoDTO> lista() {
+        Topico topico = new
+                Topico("Duvida", "duvidas com spring", new Curso("spring", "prog"));
+        return TopicoDTO.converter(Arrays.asList(topico));
+
+    }
+}
