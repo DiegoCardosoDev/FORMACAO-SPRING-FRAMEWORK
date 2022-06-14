954f9acd1b9da23b14dd7ba12533f222d8ae1c27
Diego-Cardoso <diegocardososacramento@gmail.com>
2022-06-14 02:52:40 -0300

    monitoramento implements

--- SPRING-REST-API/forum/src/main/java/br/com/alura/forum/config/security/SweggerConfigurations.java
+++ SPRING-REST-API/forum/src/main/java/br/com/alura/forum/config/security/SweggerConfigurations.java
@@ -2,22 +2,34 @@ package br.com.alura.forum.config.security;
 
 import br.com.alura.forum.modelo.Usuario;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
 
 public class SweggerConfigurations {
 
 
-
     @Bean
-    public Docket forumApi(){
+    public Docket forumApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Usuario.class);
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .description("header para token Jwt")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()
+                ));
     }
 }
